0c2e6e4bce0b9815f1a3c888e716d357db90bb22
Christoph Niethammer <christoph.niethammer@gmail.com>
2014-12-04 15:24:55 +0100

    Fix wrong variable name

--- test.sh
+++ test.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 npads="0 8 16 24 32 40 56 120"
+#npads="0 8 16 24 32 40 56 120 128 248 256"
 
 for pad in ${npads}
 do
 	echo -n "Padding: ${pad} ... "
     START=$(date +%s.%N)
-	make -B PAD=${pad} cache-analyse > /dev/null
+	make -B NPAD=${pad} cache-analyse > /dev/null
 	./cache-analyse
     END=$(date +%s.%N)
     DIFF=$(echo "$END - $START" | bc)
